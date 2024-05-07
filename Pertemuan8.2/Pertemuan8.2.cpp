4db4a4911f3055d695ee2cedb1de588f7dd97558
LichtPUBLIC <daffaalyandra65@gmail.com>
2024-05-07 10:59:01 +0700

    membuat isi dari int main

--- Pertemuan8.2/Pertemuan8.2.cpp
+++ Pertemuan8.2/Pertemuan8.2.cpp
@@ -6,7 +6,15 @@ public:
 	int nim;
 	void showNim()
 	{
-		cout << "No Induk = " << nim < endl;
+		cout << "No Induk = " << nim << endl;
 	}
 };
 
+int main()
+{
+	mahasiswa* mhs = new mahasiswa{ 1 };
+	mhs->nim = 2;
+	mhs->showNim();
+	delete mhs;
+	return 0;
+}
